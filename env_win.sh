a9760dcf682561621df7517fa356ac9ebaed5b69
unknown <Rene@Rene_Mobile.(none)>
2012-01-08 18:45:44 +0100

    Add win ev script.
    to call with:
    >source env_win.sh

---
+++ env_win.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export PATH="$PATH:/c/Program Files/CMake 2.8/bin\
+	:/c/Program Files/boost/boost_1_47/lib\
+	:/c/Program Files/OpenCV2.3.1/opencv/lib/Debug\
+	:/c/Program Files/OpenCV2.3.1/opencv/build/x86/vc9/bin\
+	:/c/Users/Rene/kinect-bridge/.build/lib/Debug"
